a43855d887bbcca64889db57006143f10e17628e
Andrei Corbu <andreicorbu7@gmail.com>
2024-02-13 22:12:22 +0200

    Added new projects

---
+++ src/Web/Settings/Dependencies.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.Extensions.Options;
+using Swashbuckle.AspNetCore.SwaggerGen;
+using System.Reflection;
+
+namespace Web.Settings
+{
+    public class Dependencies
+    {
+        public static void Inject(WebApplicationBuilder app)
+        {
+            app.Services.AddControllers();
+            app.Services.AddEndpointsApiExplorer();
+            app.Services.AddSwaggerGen(c =>
+            {
+                var xmlFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+                var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
+                c.IncludeXmlComments(xmlPath);
+            });
+            app.Services.AddTransient<IConfigureOptions<SwaggerGenOptions>, ConfigureSwaggerOptions>();
+            app.Services.AddCors(options =>
+            {
+                options.AddPolicy("Access-Control-Allow-Origin",
+                    builder =>
+                    {
+                        builder.WithOrigins("http://localhost:4200")
+                            .AllowAnyMethod()
+                            .AllowAnyHeader()
+                            .AllowCredentials();
+                    });
+            });
+            AddRepositories(app.Services);
+            AddServices(app.Services);
+        }
+
+        private static void AddRepositories(IServiceCollection services)
+        {
+            
+        }
+
+        private static void AddServices(IServiceCollection services)
+        {
+            
+        }
+    }
+}
