cb4797c95d3c29a79a0c6a05e5f2147d9b9b5e9c
Andrei Corbu <91692180+andreicorbu1@users.noreply.github.com>
2024-11-04 21:49:01 +0200

    Update environment.development.ts

--- FiscalFlow/fiscalflow.client/src/environments/environment.development.ts
+++ FiscalFlow/fiscalflow.client/src/environments/environment.development.ts
@@ -2,6 +2,6 @@ export const environment = {
   production: false,
   appUrl: 'http://localhost:5098',
   userKey: 'fiscalFlowUser',
-  openaiKey: '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',
-  facebookKey: '450918737272883',
+  openaiKey: '',
+  facebookKey: '',
 };
